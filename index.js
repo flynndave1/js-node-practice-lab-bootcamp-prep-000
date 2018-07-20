9a3c03517752a125cadbccdc5a02ad6ce5b7189b
david m flynn <flynndave1@msn.com>
2018-07-20 14:19:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // write your code below!
 
 var name = "David"
-var height = "71"
+var height = "71";
 
-console.log("Name:")
-console.log(name)
+console.log("Name:");
+console.log(name);
 
-console.log("Height:")
-console.log(height)
+console.log("Height:");
+console.log(height);
 
 console.log("New Height: " & parseInt(height) + 1);
